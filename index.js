0ef344cfc5a60671710b996eda4cad85142e73a5
TKTK123456 <103334266+TKTK123456@users.noreply.github.com>
2025-08-20 06:24:26 -0400

    Updating...

--- index.js
+++ index.js
@@ -1,5 +1,2 @@
 import { TableProxy } from './db-proxy.js'
 const Info = await new TableProxy({dbUrl: process.env.DATABASE_URL, table: 'info', logging: true})
-console.log(Info.rows)
-Info.rows[0].extra.filesInfo.end = 'today'
-setTimeout(()=>console.log(Info.rows), 1000)
